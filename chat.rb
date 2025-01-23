6f97a9bcf3c229c580a83ed21f413e9fce02e8ae
iscrol <116039611+iscrol@users.noreply.github.com>
2025-01-23 20:51:48 +0000

    completed open ai demo lesson

--- chat.rb
+++ chat.rb
@@ -1,1 +1,35 @@
-# Write your solution here!
+require "openai"
+require "dotenv/load"
+
+client=OpenAI::Client.new(access_token: ENV.fetch("OPENAI_API_KEY"))
+
+message_list = [
+  {
+    "role" => "system",
+    "content" => "You are a helpful assistant."
+  }
+]
+
+pp "Hello! How can I help you today?"
+pp "--------------------------------------------------"
+user_input = gets.chomp
+
+while user_input != "bye"
+  request = {
+    "role" => "system",
+    "content" => user_input
+  }
+ 
+  message_list.push(request)
+
+  api_response = client.chat(
+    parameters: {
+      model: "gpt-3.5-turbo",
+      messages: message_list
+    }
+  )
+
+  pp api_response["choices"][0]["message"]["content"]
+  pp "--------------------------------------------------"
+  user_input = gets.chomp
+end
